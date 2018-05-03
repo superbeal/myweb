9ff247ecbe6c315427b04fbc59d4208141a821de
bealman <447717497@qq.com>
2018-05-03 11:32:17 +0800

    update

--- myweb/settings.py
+++ myweb/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'gy95sp6qqnw0d!_kf+83+(x&0r#hnx%22os-3m+q(byh_(urww'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
---
+++ zw_app1/apps.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class ZwApp1Config(AppConfig):
+    name = 'zw_app1'
